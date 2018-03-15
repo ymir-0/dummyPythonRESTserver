2c119bce1ed4cf5b1edb135256e65486dfd4c29d
ymir <mazieres.pierre@gmail.com>
2018-03-15 19:13:43 +0100

    non secured service

--- DummyPythonRESTserver.py
+++ DummyPythonRESTserver.py
@@ -1,1 +1,28 @@
-# see REST with Python tuto : https://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
+#!/usr/bin/env python3
+'''
+see REST with Python help :
+ - https://flask-restful.readthedocs.io/en/latest/quickstart.html
+ - https://stackoverflow.com/questions/30779584/flask-restful-passing-parameters-to-get-request
+'''
+from flask import Flask, jsonify, request
+# initialize application
+app = Flask(__name__)
+# index
+@app.route('/')
+def index():
+    return "Hello, World!"
+# dummiest WS
+@app.route('/test/dummiest', methods=['POST','PUT','GET','DELETE'])
+def dummiest():
+    return jsonify(
+        {
+            'host': request.host,
+            'path': request.path,
+            'method': request.method,
+            'args': request.args,
+            'jsonData': request.json
+        }
+    )
+# main
+if __name__ == '__main__':
+    app.run()
--- setup.py
+++ setup.py
@@ -4,14 +4,9 @@ import os
 import setuptools
 
 module_path = os.path.join(os.path.dirname(__file__), 'DummyPythonRESTserver.py')
-version_line = [line for line in open(module_path)
-                if line.startswith('__version__')][0]
-
-__version__ = version_line.split('__version__ = ')[-1][1:][:-2]
 
 setuptools.setup(
     name="DummyPythonRESTserver",
-    version=__version__,
     description="Dummy test for REST with Python3",
     py_modules=['DummyPythonRESTserver'],
     install_requires=[],
